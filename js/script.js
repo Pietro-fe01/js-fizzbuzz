bc6d161b9d38707b5c2d0f0c2f711da7f34add5b
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-24 17:05:26 +0200

    Implementata anche la parte grafica del programma: - Per ogni numero generato viene creato un div - All'interno di ogni div viene stampato la parola "fizzbuzz", "fizz", "Buzz" o il numero in base agli if

--- js/script.js
+++ js/script.js
@@ -4,18 +4,6 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i
 multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 
 che di 5 stampi “FizzBuzz”.*/
 
-for(let i = 1; i<=100; i++){
-    if ( i % 3 === 0 && i % 5 === 0 ){
-        console.log("FizzBuzz");
-    } else if ( i % 3 === 0){
-        console.log("Fizz");
-    } else if ( i % 5 === 0){
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
-
 /*BONUS 1: Crea un container nel DOM , aggiungendo 
 (attraverso la funzione append()) un elemento html con il numero o la 
 stringa corretta da mostrare.*/
@@ -23,3 +11,32 @@ stringa corretta da mostrare.*/
 /*BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1,
 a seconda che il valore inserito sia un numero, un fizz, 
 un buzz o un fizzbuzz. */
+
+
+const fizzBuzzContainer = document.querySelector(".fizzbuzz-container");
+let fizzBuzzItem;
+
+for(let i = 1; i<=100; i++){
+    if ( i % 3 === 0 && i % 5 === 0 ){
+        fizzBuzzItem = document.createElement("div");
+        fizzBuzzItem.classList.add("fizzbuzz-item", "fizzbuzz-bk-color");
+        fizzBuzzItem.innerHTML = "FizzBuzz";
+        fizzBuzzContainer.append(fizzBuzzItem);
+    } else if ( i % 3 === 0){
+        fizzBuzzItem = document.createElement("div");
+        fizzBuzzItem.classList.add("fizzbuzz-item", "fizz-bk-color");
+        fizzBuzzItem.innerHTML = "Fizz";
+        fizzBuzzContainer.append(fizzBuzzItem);
+    } else if ( i % 5 === 0){
+        fizzBuzzItem = document.createElement("div");
+        fizzBuzzItem.classList.add("fizzbuzz-item", "buzz-bk-color");
+        fizzBuzzItem.innerHTML = "Buzz";
+        fizzBuzzContainer.append(fizzBuzzItem);
+    } else {
+        fizzBuzzItem = document.createElement("div");
+        fizzBuzzItem.classList.add("fizzbuzz-item", "blue-bk-color");
+        fizzBuzzItem.innerHTML = i;
+        fizzBuzzContainer.append(fizzBuzzItem);
+    }
+}
+
